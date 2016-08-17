ec640f44b7e5300d7399f704c50e9431ccd08366
fireflyyx <fireflyyx@163.com>
2016-08-17 16:40:11 +0800

    mainBranch

--- app/src/main/java/com/neo/http/myviewtest/MainActivity.java
+++ app/src/main/java/com/neo/http/myviewtest/MainActivity.java
@@ -2,7 +2,6 @@ package com.neo.http.myviewtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,16 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.countview);
-
-        RelativeLayout bt= (RelativeLayout) findViewById(R.id.relativelayout);
-//
-//
-//        //获得LayoutInflater实例，简化写法
-////        LayoutInflater inflater=LayoutInflater.from(this);
-//        //获得layoutInflater实例，完整写法
-//        LayoutInflater inflater1= (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//
-//        View view=inflater1.inflate(R.layout.twopage,null);
-//        bt.addView(view);
     }
 }
